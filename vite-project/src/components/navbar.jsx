df8d4875f9901381e53c470daade9369bbead0bb
jasnmar <jasnmar@gmail.com>
2024-04-18 10:27:24 -0400

    Main Page

--- vite-project/src/components/navbar.jsx
+++ vite-project/src/components/navbar.jsx
@@ -4,22 +4,16 @@ import "./Navbar.css"
 
 function Navbar() {
     return (
-        <div className="nav">
+        <nav className="nav">
             <div className="logo">
                 <img className="logo-image" src={reactLogo}></img>
                 <p className="title">Navbar Here</p>
             </div>
             <p className="extra-title">React Course - Project 1</p>
-        </div>
+        </nav>
         
     )
 }
 
 export default Navbar
 
-/**
-Challenge: Complete the Navbar to match the design
-
-Hint: use the Figma file for the most accurate peek at the design
-(colors, sizes, fonts, etc.)
-*/
